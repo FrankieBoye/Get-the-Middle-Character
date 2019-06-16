210f53f7974f689a044b909648553e6111c5bfc2
FrankieBoye <frankie4@btinternet.com>
2019-06-16 07:46:57 +0100

    added coverage

--- spec/middle_spec.rb
+++ spec/middle_spec.rb
@@ -1,12 +1,11 @@
 require 'middle'
 
-  describe '#middle' do
-    it 'returns the middle character when passed a word with an odd number of letters' do
-      expect(middle("testing")).to eq 't'
+describe '#middle' do
+  it 'returns the middle character when passed an odd number of letters' do
+    expect(middle('testing')).to eq 't'
   end
 
-    it 'returns the middle 2 characters when passed a word with an even number of letters' do
-      expect(middle("test")).to eq 'es'
-    end
-
+  it 'returns the middle 2 characters when passed an even number of letters' do
+    expect(middle('test')).to eq 'es'
+  end
 end
